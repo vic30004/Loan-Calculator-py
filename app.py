40344da2845f8873b39fbea15f08046f980adb49
vic30004 <victor_993@msn.com>
2020-01-28 11:11:33 +0200

    added the text boxes for user input

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ class LoanCalculator:
         window = Tk()
         window.title("Loan Calculator")
         window.configure(background="black")
+        Label(window, font='Helvetica 12 bold', bg="light green",
+              text="Annual Interest Rate").grid(row=1, column=1, sticky=W)
+        Label(window, font="Helvetica 12 bold", Text="Number of Years").grid(
+            row=2, column=1, sticky=W)
+        Label(window, font="Helvetica 12 bold", Text="Loan Amount").grid(
+            row=3, column=1, sticky=W)
+        Label(window, font="Helvetica 12 bold", Text="Monthly Payment").grid(
+            row=4, column=1, sticky=W)
+        Label(window, font="Helvetica 12 bold", Text="Total Payment").grid(
+            row=5, column=1, sticky=W)
