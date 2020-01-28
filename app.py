40f3b4f8ff717b1235bbafbb2e9743be3755b711
vic30004 <victor_993@msn.com>
2020-01-28 11:20:17 +0200

    added inputs for the labels previously added

--- app.py
+++ app.py
@@ -6,13 +6,34 @@ class LoanCalculator:
         window = Tk()
         window.title("Loan Calculator")
         window.configure(background="black")
+        # Labels for inputs
         Label(window, font='Helvetica 12 bold', bg="light green",
               text="Annual Interest Rate").grid(row=1, column=1, sticky=W)
-        Label(window, font="Helvetica 12 bold", Text="Number of Years").grid(
+        Label(window, font="Helvetica 12 bold", text="Number of Years").grid(
             row=2, column=1, sticky=W)
-        Label(window, font="Helvetica 12 bold", Text="Loan Amount").grid(
+        Label(window, font="Helvetica 12 bold", text="Loan Amount").grid(
             row=3, column=1, sticky=W)
-        Label(window, font="Helvetica 12 bold", Text="Monthly Payment").grid(
+        Label(window, font="Helvetica 12 bold", text="Monthly Payment").grid(
             row=4, column=1, sticky=W)
-        Label(window, font="Helvetica 12 bold", Text="Total Payment").grid(
+        Label(window, font="Helvetica 12 bold", text="Total Payment").grid(
             row=5, column=1, sticky=W)
+
+        # Inputs
+        self.annualInterestRateVar = StringVar()
+        Entry(window, textvariable=self.annualInterestRateVar,
+              justify=RIGHT).grid(row=1, column=2)
+        self.numberofYearsVar = StringVar()
+        Entry(window, textvariable=self.numberofYearsVar,
+              justify=RIGHT).grid(row=2, column=2)
+        self.loanAmountVar = StringVar()
+        Entry(window, textvariable=self.loanAmountVar,
+              justify=RIGHT).grid(row=3, column=2)
+        self.monthlyPaymentVar = StringVar()
+        lblMonthlyPayment = Label(window, textvariable=self.monthlyPaymentVar,
+                                  justify=RIGHT).grid(row=4, column=2, sticky=E)
+        self.totalPaymentVar = StringVar()
+        lblTotalPayment = Label(window, font='Helvetica 12 bold', bg="light green",
+                                textvariable=self.totalPaymentVar).grid(row=5, column=2, sticky=E)
+
+
+LoanCalculator()
